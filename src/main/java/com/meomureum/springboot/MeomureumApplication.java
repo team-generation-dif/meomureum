f912ae0210c1a7d6acca898a0a47e198e679d4a6
KWJ <kang65516311@gmail.com>
2026-01-06 12:43:37 +0900

    asdasd

---
+++ src/main/java/com/meomureum/springboot/MeomureumApplication.java
@@ -0,0 +1,13 @@
+package com.meomureum.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeomureumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeomureumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meomureum/springboot/MeomureumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meomureum.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeomureumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
